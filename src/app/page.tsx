99caf2bb44be32f3ad4de31b8982f9ffd243a795
yakir3160 <132565923+yakir3160@users.noreply.github.com>
2025-02-20 14:34:57 +0200

    Add VSCode debug configuration and refine page layout with overflow and responsive adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,21 @@ import { useChatStore } from '@/src/store/chatStore';
 export default function AdvancedChatbot() {
     const { showHistory } = useChatStore();
     return (
-        <div className="flex flex-col ">
+        <div className="flex flex-col">
             <Header/>
-            <div className="flex mt-24 h-[calc(100vh-14rem)] md:h-[calc(100vh-6rem)] overflow-hidden  ">
+            <div className="flex mt-24 h-[calc(100vh-14rem)] md:h-[calc(100vh-6rem)]">
                 <Sidebar />
                 <ToggleSidebarButton />
                 {/* Main Content */}
-                <div className={`w-full flex justify-center  `}>
+                <div className={`w-full flex justify-center`}>
                     <main className={`
-                    
                         flex-1 flex flex-col justify-end
-                        h-[calc(100vh-10rem)] md:h-[calc(100vh-6rem)]
+                        h-[calc(100vh-10rem)] md:h-[calc(100svh-6rem)]
                         transition-all duration-300 pt-2
-                        max-w-7xl w-full 
+                        max-w-7xl w-full
                         ${showHistory ? 'lg:ml-0' : 'lg:mx-4'}
                     `}>
-                        <div className="px-1 md:px-12 space-y-2 flex flex-col  ">
+                        <div className="px-1 md:px-12 overflow-y-auto space-y-2 flex flex-col flex-grow">
                             <ChatInterface />
                             <ChatInput />
                         </div>
