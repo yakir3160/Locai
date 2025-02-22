547ebb6e3c21a471957835d8fcf33b5c0fcb5d68
yakir3160 <132565923+yakir3160@users.noreply.github.com>
2025-02-22 10:52:35 +0200

    Refactor Logo component to remove unused code and update HomeIcon size

--- components/ui/custom/Logo.tsx
+++ components/ui/custom/Logo.tsx
@@ -8,14 +8,9 @@ export const Logo = () => {
       <div className="bg-gradient-ai text-transparent bg-clip-text logo ">
         <Link href="/" className="flex flex-row justify-normal items-center gap-2">
           <span className="text-3xl md:text-4xl font-bold">locai</span>
-          <HomeIcon className="size-7 md:size-9 fill-[url(#logo-gradient)] " />
+          <HomeIcon className="size-7 md:size-9" />
         </Link>
-        <svg className="size-0 ">
-          <linearGradient id="logo-gradient">
-            <stop offset="0%" stopColor="#ec4899" />
-            <stop offset="100%" stopColor="#f97316" />
-          </linearGradient>
-        </svg>
+        
       </div>
     </div>
   );
